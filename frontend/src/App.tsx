7323ad83dccf3cc9d9c75256d37d78f1d8178aaa
airbus <001airbus@gmail.com>
2025-06-03 20:34:40 +0900

    chore: 병합전 커밋
    - App.tsx에 추가했던 불필요한 부분 제거
    - socket.io 설치

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,41 +1,12 @@
-import { useState } from "react";
 import ChatRoom from "./pages/ChatRoom";
-import VoteModal from "./pages/components/VoteModal";
 import { GlobalStyle } from "./styles/global.style";
-import Footer from "./components/Footer";
 
 function App() {
-
-  // 투표 모달 상태관리
-  const [isVoteModalOpen, setIsVoteModalOpen] = useState(false);
-  const [voteMode, setVoteMode] = useState<'create' | 'owner' | 'participant'>('create');
-
-  // 퀴즈 상태관리
-  const [chatAutoInput, setChatAutoInput] = useState("");
 
   return (
     <>
       <GlobalStyle />
       <ChatRoom />
-
-      {/* 투표 (생성/수정/투표자) 테스트 버튼 */}
-      <div style={{display:'flex', gap: 8, marginBottom: 16}}>
-        <button onClick={() => setVoteMode('create')}>Create 모드</button>
-        <button onClick={() => setVoteMode('owner')}>Owner 모드</button>
-        <button onClick={() => setVoteMode('participant')}>Participant 모드</button>
-      </div>
-      <button onClick={()=>setIsVoteModalOpen(true)}>투표 만들기</button>
-      <VoteModal
-        isOpen={isVoteModalOpen}
-        onClose={() => setIsVoteModalOpen(false)}
-        mode={voteMode}
-      />
-
-      <Footer 
-        openVoteModal={() => setIsVoteModalOpen(true)}
-        chatAutoInput={chatAutoInput}
-        setChatAutoInput={setChatAutoInput}
-      />
     </>
   )
 }
